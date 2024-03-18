8a44bc88c4091fe754bff393e2a52d6546b2be84
dlgoodr <dlg@dsrw.org>
2024-03-18 14:16:47 -0500

    stop cloud migration

--- terraform/core_infra/iam.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_role" "wbip_wrapper_role" {
-  name               = "wbip_wrapper_exec"
-  assume_role_policy = file("${path.root}/../../iam/roles/wbip_wrapper_exec.json")
-  inline_policy {
-    name   = "sort_instapaper_exec"
-    policy = file("${path.root}/../../iam/policies/wbip_wrapper_exec.json")
-  }
-}
